e99c404df2dd38958411311813828b0817df0bb5
demir09 <demir@fim.uni-passau.de>
2026-08-08 16:39:47 +0200

    fix: restructure dashboard and implement 4 visualization charts

--- dashboard.js
+++ dashboard.js
@@ -9,268 +9,193 @@
 * All rights reserved.
 */
 
-// Use this website as a motivation: https://observablehq.com/framework/examples/eia/
-// TODO: File for Part 2
-// TODO: You can edit this file as you wish - add new methods, variables etc. or change/delete existing ones.
-
-// TODO: use descriptive names for variables
 let chart1, chart2, chart3, chart4;
+let gapminderInterval;
 
 function initDashboard(_data) {
-    // Define width and height for SVG containers
-    const width = 975;
-    const height = 610;
+    const width = 600;
+    const height = 400;
 
-    // Initialize the environment (SVG, etc.) and call the needed methods
-    chart1 = d3.select("#chart1").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g");
-        /*
-*    Mastering Data Visualization with D3.js
-*    Gapminder Clone
-*/
-
-document.getElementById('upload').addEventListener('change',function(event){
-    if(file){
-        const reader = new FileReader();
-        reader.onload=function(e){
-            const csvData = e.target.result;
-            const data=d3.csvParse(csvData);
-            console.log(data);
-        };
-        // reader.readAsText(file);
+    // Clear old charts and intervals if any
+    if (gapminderInterval) {
+        gapminderInterval.stop();
     }
-});
+    d3.select("#chart1").selectAll("*").remove();
+    d3.select("#chart2").selectAll("*").remove();
+    d3.select("#chart3").selectAll("*").remove();
+    d3.select("#chart4").selectAll("*").remove();
 
-var margin = { left:80, right:20, top:50, bottom:100 };
-// var height = 500 - margin.top - margin.bottom, 
-//     width = 800 - margin.left - margin.right;
+    chart1 = d3.select("#chart1").append("svg").attr("width", width).attr("height", height).append("g");
+    chart2 = d3.select("#chart2").append("svg").attr("width", width).attr("height", height).append("g");
+    chart3 = d3.select("#chart3").append("svg").attr("width", width).attr("height", height).append("g");
+    chart4 = d3.select("#chart4").append("svg").attr("width", width).attr("height", height).append("g");
 
-var g = d3.select("#chart-area")
-    .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform", "translate(" + margin.left + 
-            ", " + margin.top + ")");
+    createChart1(_data, width, height); // Gap Minder Clone
+    createChart2(_data, width, height); // Bubble Chart
+    createChart3(_data, width, height); // Bar Chart
+    createChart4(_data, width, height); // Line/Scatter Chart
+}
 
-var time = 0;
+// Chart 1: Gap Minder Clone (uses its own dataset as it requires specific structure)
+function createChart1(_data, width, height) {
+    const margin = { left: 60, right: 20, top: 20, bottom: 60 };
+    const w = width - margin.left - margin.right;
+    const h = height - margin.top - margin.bottom;
 
-// Scales
-var x = d3.scaleLog()
-    .base(10)
-    .range([0, width])
-    .domain([142, 150000]);
-var y = d3.scaleLinear()
-    .range([height, 0])
-    .domain([0, 90]);
-var area = d3.scaleLinear()
-    .range([25*Math.PI, 1500*Math.PI])
-    .domain([2000, 1400000000]);
-var continentColor = d3.scaleOrdinal(d3.schemePastel1);
+    const g = chart1.attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-// Labels
-var xLabel = g.append("text")
-    .attr("y", height + 50)
-    .attr("x", width / 2)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .text("GDP Per Capita ($)");
-var yLabel = g.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", -40)
-    .attr("x", -170)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .text("Life Expectancy (Years)")
-var timeLabel = g.append("text")
-    .attr("y", height -10)
-    .attr("x", width - 40)
-    .attr("font-size", "40px")
-    .attr("opacity", "0.4")
-    .attr("text-anchor", "middle")
-    .text("1800");
+    let time = 0;
+    const x = d3.scaleLog().base(10).range([0, w]).domain([142, 150000]);
+    const y = d3.scaleLinear().range([h, 0]).domain([0, 90]);
+    const area = d3.scaleLinear().range([25 * Math.PI, 1500 * Math.PI]).domain([2000, 1400000000]);
+    const continentColor = d3.scaleOrdinal(d3.schemePastel1);
 
-// X Axis
-var xAxisCall = d3.axisBottom(x)
-    .tickValues([400, 4000, 40000])
-    .tickFormat(d3.format("$"));
-g.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height +")")
-    .call(xAxisCall);
+    const xLabel = g.append("text").attr("y", h + 40).attr("x", w / 2).attr("font-size", "14px").attr("text-anchor", "middle").text("GDP Per Capita ($)");
+    const yLabel = g.append("text").attr("transform", "rotate(-90)").attr("y", -40).attr("x", -h / 2).attr("font-size", "14px").attr("text-anchor", "middle").text("Life Expectancy (Years)");
+    const timeLabel = g.append("text").attr("y", h - 10).attr("x", w - 40).attr("font-size", "30px").attr("opacity", "0.4").attr("text-anchor", "middle").text("1800");
 
-// Y Axis
-var yAxisCall = d3.axisLeft(y)
-    .tickFormat(function(d){ return +d; });
-g.append("g")
-    .attr("class", "y axis")
-    .call(yAxisCall);
+    g.append("g").attr("class", "x axis").attr("transform", "translate(0," + h + ")").call(d3.axisBottom(x).tickValues([400, 4000, 40000]).tickFormat(d3.format("$")));
+    g.append("g").attr("class", "y axis").call(d3.axisLeft(y));
 
-d3.json('https://raw.githubusercontent.com/ioanmeri/resources/master/GapminderClone.json').then(function(data){
-    console.log(data);
+    d3.json('https://raw.githubusercontent.com/ioanmeri/resources/master/GapminderClone.json').then(function(data) {
+        const formattedData = data.map(function(year) {
+            return year["countries"].filter(function(country) {
+                return (country.income && country.life_exp);
+            }).map(function(country) {
+                country.income = +country.income;
+                country.life_exp = +country.life_exp;
+                return country;
+            })
+        });
 
-    // Clean data
-    const formattedData = data.map(function(year){
-        return year["countries"].filter(function(country){
-            var dataExists = (country.income && country.life_exp);
-            return dataExists
-        }).map(function(country){
-            country.income = +country.income;
-            country.life_exp = +country.life_exp;
-            return country;            
-        })
+        function update(data) {
+            const t = d3.transition().duration(100);
+            const circles = g.selectAll("circle").data(data, d => d.country);
+
+            circles.exit().remove();
+
+            circles.enter().append("circle")
+                .attr("fill", d => continentColor(d.continent))
+                .merge(circles)
+                .transition(t)
+                .attr("cy", d => y(d.life_exp))
+                .attr("cx", d => x(d.income))
+                .attr("r", d => Math.sqrt(area(d.population) / Math.PI));
+
+            timeLabel.text(+(time + 1800));
+        }
+
+        update(formattedData[0]);
+
+        gapminderInterval = d3.interval(function() {
+            time = (time < 214) ? time + 1 : 0;
+            update(formattedData[time]);
+        }, 100);
     });
-
-    // Run the code every 0.1 second
-    d3.interval(function(){
-        // At the end of our data, loop back
-        time = (time < 214) ? time+1 : 0
-        update(formattedData[time]);            
-    }, 100);
-
-    // First run of the visualization
-    update(formattedData[0]);
-
-})
-
-function update(data) {
-    // Standard transition time for the visualization
-    var t = d3.transition()
-        .duration(100);
-
-    // JOIN new data with old elements.
-    var circles = g.selectAll("circle").data(data, function(d){
-        return d.country;
-    });
-
-    // EXIT old elements not present in new data.
-    circles.exit()
-        .attr("class", "exit")
-        .remove();
-
-    // ENTER new elements present in new data.
-    circles.enter()
-        .append("circle")
-        .attr("class", "enter")
-        .attr("fill", function(d) { return continentColor(d.continent); })
-        .merge(circles)
-        .transition(t)
-            .attr("cy", function(d){ return y(d.life_exp); })
-            .attr("cx", function(d){ return x(d.income) })
-            .attr("r", function(d){ return Math.sqrt(area(d.population) / Math.PI) });
-
-    // Update the time label
-    timeLabel.text(+(time + 1800))
 }
 
-    chart2 = d3.select("#chart2").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g");
+// Chart 2: Generic Bubble Chart using uploaded data
+function createChart2(_data, width, height) {
+    if (!_data || _data.length === 0) return;
+    
+    const margin = { left: 50, right: 20, top: 20, bottom: 40 };
+    const w = width - margin.left - margin.right;
+    const h = height - margin.top - margin.bottom;
+    const g = chart2.attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-    chart3 = d3.select("#chart3").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g");
+    const cols = Object.keys(_data[0]);
+    if (cols.length < 2) return;
 
-    chart4 = d3.select("#chart4").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g");
+    // Use first two columns for X and Y, optionally third for size
+    const xCol = cols[1];
+    const yCol = cols.length > 2 ? cols[2] : cols[1];
+    const sizeCol = cols.length > 3 ? cols[3] : null;
 
-    createChart1(_data);
-    createChart2(_data);
-    createChart3(_data);
-    createChart4(_data);
+    const x = d3.scaleLinear().domain([0, d3.max(_data, d => +d[xCol]) || 10]).range([0, w]);
+    const y = d3.scaleLinear().domain([0, d3.max(_data, d => +d[yCol]) || 10]).range([h, 0]);
+    
+    let r = d3.scaleLinear().range([5, 5]);
+    if (sizeCol) {
+        r.domain([0, d3.max(_data, d => +d[sizeCol]) || 10]).range([3, 15]);
+    }
+
+    g.append("g").attr("transform", "translate(0," + h + ")").call(d3.axisBottom(x));
+    g.append("g").call(d3.axisLeft(y));
+    
+    g.append("text").attr("y", h + 30).attr("x", w / 2).attr("text-anchor", "middle").text(xCol);
+    g.append("text").attr("transform", "rotate(-90)").attr("y", -35).attr("x", -h / 2).attr("text-anchor", "middle").text(yCol);
+
+    g.selectAll("circle").data(_data).enter().append("circle")
+        .attr("cx", d => x(+d[xCol]) || 0)
+        .attr("cy", d => y(+d[yCol]) || 0)
+        .attr("r", d => sizeCol ? r(+d[sizeCol]) || 5 : 5)
+        .attr("fill", "steelblue")
+        .attr("opacity", 0.7);
 }
 
-function createChart1(_data) {
-    const data = _data.population.map((d) => ({
-        ...d,
-        county: _data.countymap.get(d.fips),
-        state: _data.statemap.get(d.state)
-    }))
-    .filter(d => d.county)
-    .sort((a, b) => d3.descending(a.population, b.population));
+// Chart 3: Generic Bar Chart using uploaded data
+function createChart3(_data, width, height) {
+    if (!_data || _data.length === 0) return;
+    
+    const margin = { left: 50, right: 20, top: 20, bottom: 80 };
+    const w = width - margin.left - margin.right;
+    const h = height - margin.top - margin.bottom;
+    const g = chart3.attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-    const radius = d3.scaleSqrt()
-        .domain([0, d3.max(data, d => d.population)])
-        .range([0, 40]);
+    const cols = Object.keys(_data[0]);
+    const labelCol = cols[0];
+    const valCol = cols[1] || cols[0];
 
-    const path = d3.geoPath();
+    const data = _data.slice(0, 15); // Limit to 15 items for readability
 
-    const svg = d3.select("#chart1 svg")
-        .attr("viewBox", [0, 0, 975, 610])
-        .attr("style", "width: 100%; height: auto; height: intrinsic;");
+    const x = d3.scaleBand().range([0, w]).domain(data.map(d => d[labelCol])).padding(0.2);
+    const y = d3.scaleLinear().domain([0, d3.max(data, d => +d[valCol]) || 10]).range([h, 0]);
 
-    svg.append("path")
-        .datum(topojson.feature(_data.us, _data.us.objects.nation))
-        .attr("fill", "#ddd")
-        .attr("d", path);
+    g.append("g").attr("transform", "translate(0," + h + ")").call(d3.axisBottom(x))
+     .selectAll("text").attr("transform", "translate(-10,0)rotate(-45)").style("text-anchor", "end");
+    g.append("g").call(d3.axisLeft(y));
 
-    svg.append("path")
-        .datum(topojson.mesh(_data.us, _data.us.objects.states, (a, b) => a !== b))
+    g.append("text").attr("transform", "rotate(-90)").attr("y", -35).attr("x", -h / 2).attr("text-anchor", "middle").text(valCol);
+
+    g.selectAll("rect").data(data).enter().append("rect")
+        .attr("x", d => x(d[labelCol]))
+        .attr("y", d => y(+d[valCol]) || 0)
+        .attr("width", x.bandwidth())
+        .attr("height", d => h - (y(+d[valCol]) || 0))
+        .attr("fill", "#69b3a2");
+}
+
+// Chart 4: Generic Line Chart using uploaded data
+function createChart4(_data, width, height) {
+    if (!_data || _data.length === 0) return;
+    
+    const margin = { left: 50, right: 20, top: 20, bottom: 40 };
+    const w = width - margin.left - margin.right;
+    const h = height - margin.top - margin.bottom;
+    const g = chart4.attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+    const cols = Object.keys(_data[0]);
+    const xCol = cols[0];
+    const yCol = cols.length > 1 ? cols[1] : cols[0];
+
+    const data = _data.slice(0, 50);
+
+    const x = d3.scalePoint().domain(data.map(d => d[xCol])).range([0, w]);
+    const y = d3.scaleLinear().domain([0, d3.max(data, d => +d[yCol]) || 10]).range([h, 0]);
+
+    g.append("g").attr("transform", "translate(0," + h + ")").call(d3.axisBottom(x).tickValues(x.domain().filter((d,i) => !(i%5))));
+    g.append("g").call(d3.axisLeft(y));
+
+    g.append("text").attr("y", h + 35).attr("x", w / 2).attr("text-anchor", "middle").text(xCol);
+    g.append("text").attr("transform", "rotate(-90)").attr("y", -35).attr("x", -h / 2).attr("text-anchor", "middle").text(yCol);
+
+    g.append("path")
+        .datum(data)
         .attr("fill", "none")
-        .attr("stroke", "white")
-        .attr("stroke-linejoin", "round")
-        .attr("d", path);
-
-    const legend = svg.append("g")
-        .attr("fill", "#777")
-        .attr("transform", "translate(915,608)")
-        .attr("text-anchor", "middle")
-        .style("font", "10px sans-serif")
-        .selectAll("g")
-        .data(radius.ticks(4).slice(1))
-        .join("g");
-
-    legend.append("circle")
-        .attr("fill", "none")
-        .attr("stroke", "#ccc")
-        .attr("cy", d => -radius(d))
-        .attr("r", radius);
-
-    legend.append("text")
-        .attr("y", d => -2 * radius(d))
-        .attr("dy", "1.3em")
-        .text(radius.tickFormat(4, "s"));
-
-    const format = d3.format(",.0f");
-    svg.append("g")
-        .attr("fill", "brown")
-        .attr("fill-opacity", 0.5)
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 0.5)
-        .selectAll("circle")
-        .data(data)
-        .join("circle")
-        .attr("transform", d => `translate(${centroid(d.county)})`)
-        .attr("r", d => radius(d.population))
-        .append("title")
-        .text(d => `${d.county.properties.name}, ${d.state.properties.name}\n${format(d.population)}`);
+        .attr("stroke", "orange")
+        .attr("stroke-width", 2)
+        .attr("d", d3.line()
+            .x(d => x(d[xCol]))
+            .y(d => y(+d[yCol]) || 0)
+        );
 }
-
-function createChart2(_data) {
-    // Add logic for chart 2
-}
-
-function createChart3(_data) {
-    // Add logic for chart 3
-}
-
-function createChart4(_data) {
-    // Add logic for chart 4
-}
-
-// Clear charts if changes (dataset) occur
-function clearDashboard() {
-    chart1.selectAll("*").remove();
-    chart2.selectAll("*").remove();
-    chart3.selectAll("*").remove();
-    chart4.selectAll("*").remove();
-}
-
-
-
